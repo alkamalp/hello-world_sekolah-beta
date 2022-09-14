b8a3e67c7e3a28a91629282b4a9b34863fed05de
alkamalp <alkamalp@gmail.com>
2022-09-14 18:04:51 +0700

    separate widgets

---
+++ lib/hellohome.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'hellowidget.dart';
+
+class HelloHome extends StatelessWidget {
+  const HelloHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation',
+        ),
+        title: const Text('Hello World'),
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: const HelloWidget(),
+    );
+  }
+}
---
+++ lib/hellowidget.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HelloWidget extends StatelessWidget {
+  const HelloWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      constraints: BoxConstraints.expand(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          Text(
+            "Hello",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40.0,
+            ),
+          ),
+          Text(
+            "World",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontSize: 60.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
