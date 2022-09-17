dccc1c75ab11e3df0545b174d5127e89a40d4337
alkamalp <alkamalp@gmail.com>
2022-09-17 09:50:06 +0700

    delete import html in hellowidget.dart

--- lib/hellowidget.dart
+++ lib/hellowidget.dart
@@ -1,9 +1,4 @@
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HelloWidget extends StatelessWidget {
   const HelloWidget({Key? key}) : super(key: key);
